686c56c793153fdc4e048031e6a81a9026dff21e
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2021-10-20 17:33:29 +0300

    money

--- money/src/App.js
+++ money/src/App.js
@@ -1,8 +1,16 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/header';
+
+
 
 function App() {
+
+  const [initialMoney, setInitialMoney] = useState( initialState: 100)
+
   return (
     <h1>Hello from app</h1>
+    <Header/>
     
   );
 }
