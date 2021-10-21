3b0e17c118af0889b2928e501e62307a64d8b3aa
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2021-10-21 09:00:52 +0300

    div added

--- money/src/App.js
+++ money/src/App.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/header';
+import Header from './components/Header';
 
 
 
 function App() {
 
-  const [initialMoney, setInitialMoney] = useState( initialState: 100)
+  const [initialMoney, setInitialMoney] = useState(100)
 
   return (
-    <h1>Hello from app</h1>
-    <Header/>
+    <div>
+      <h1>Hello from app</h1>
+      <Header/>
+    </div>
+    
     
   );
 }
